0dac07ce55502abed6b7a4f37adce37512e0e9e8
zahidhussain998 <zahidzahidhusssain@gmail.com>
2024-11-17 21:57:43 +0500

    new chnages

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
 import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
@@ -6,11 +5,10 @@ import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
 function Slider() {
   const [current, setCurrent] = useState(0);
   const data = [
-    {image1: "03.jpg"},
-    {image1: "01.jpg"},
-    {image1: "image.jpg"},
-    {image1: "1112.jpg"}
-];
+    { image1: "t1.jpeg" },
+    { image1: "Leonardo_Phoenix_I_want_to_create_a_banner_the_only_4_candles_1-transformed.jpeg" },
+    { image1: "Leonardo_Phoenix_hey_create_the_banner_D4_designer_Aroma_candl_3-transformed.jpeg" },
+  ];
 
   const prevSlide = () => {
     setCurrent(current === 0 ? data.length - 1 : current - 1);
@@ -21,7 +19,7 @@ function Slider() {
   };
 
   return (
-    <div className="relative overflow-hidden ">
+    <div className="relative overflow-hidden">
       <div className="h-[500px] sm:h-[500px] md:h-[500px] lg:h-[700px] w-full relative">
         <div className="flex w-full h-full">
           {/* Slide container */}
@@ -30,7 +28,7 @@ function Slider() {
               key={index}
               src={slide.image1}
               alt={`Slide ${index + 1}`}
-              className={`w-[100vw]  object-fit absolute transition-opacity duration-700 ease-in-out ${
+              className={`w-full h-full object-cover absolute transition-opacity duration-700 ease-in-out ${
                 index === current ? "opacity-100" : "opacity-0"
               }`}
             />
@@ -42,7 +40,7 @@ function Slider() {
       <div className="absolute inset-0 flex justify-between items-center px-4">
         {/* Left Icon */}
         <div
-          className=" bg-white bg-opacity-50 rounded-full cursor-pointer sm:p-4 lg:p-5"
+          className="bg-white bg-opacity-50 rounded-full cursor-pointer sm:p-4 lg:p-5"
           onClick={prevSlide}
         >
           <WestOutlinedIcon className="text-gray-800 sm:text-[2rem] lg:text-[2.5rem] hover:scale-110 transition-transform duration-300" />
@@ -50,7 +48,7 @@ function Slider() {
 
         {/* Right Icon */}
         <div
-          className=" bg-white bg-opacity-50 rounded-full cursor-pointer sm:p-4 lg:p-5"
+          className="bg-white bg-opacity-50 rounded-full cursor-pointer sm:p-4 lg:p-5"
           onClick={nextSlide}
         >
           <EastOutlinedIcon className="text-gray-800 sm:text-[2rem] lg:text-[2.5rem] hover:scale-110 transition-transform duration-300" />
